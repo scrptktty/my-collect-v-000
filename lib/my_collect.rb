026272a4f337e371279ab1c888606f3c147d5c24
Kachel Wittig <kjwittig@gmail.com>
2018-03-15 02:57:42 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+def my_collect(array)
+  n = 0
+  collecter = []
+  while n < array.length 
+    collecter << yield(array[n])
+    n += 1
+  end  
+  collecter
+end
 
+require 'pry'
 
+# 
+# def hello(array)
+#   i = 0
+#   collection = []
+#   while i < array.length
+#     collection << yield(array[i])
+#     i += 1
+#   end
+#   collection
+# 
+# hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
